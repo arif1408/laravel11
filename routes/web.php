a8ca2d4b6d8b25819caeaf9ac43460597a9b0598
imam <maarifbatubara@gmail.com>
2024-05-27 17:20:41 +0700

    Balde_component

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,15 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('Home');
+    return view('Home',['title'=>'Home Page']);
 });
  
 Route::get('/about', function () {
-    return view('about');
+    return view('about',['title'=>'About','name'=>'Imam Maarif']);
 });
 Route::get('/blog', function () {
-    return view('blog',['nama'=>'imam maarif']);
+    return view('blog',['title'=>'Blog']);
 });
 Route::get('/contact', function () {
-    return view('contact');
+    return view('contact',['title'=>'Contact']);
 });
